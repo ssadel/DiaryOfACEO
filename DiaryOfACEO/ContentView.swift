0c4551a960319324d8de2c22915dcba650e4ed1f
Sid Sadel <sidneysadel@gmail.com>
2024-03-07 11:51:19 -0500

    Initial Commit

---
+++ DiaryOfACEO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiaryOfACEO
+//
+//  Created by Sidney Sadel on 3/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiaryOfACEO/DiaryOfACEOApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryOfACEOApp.swift
+//  DiaryOfACEO
+//
+//  Created by Sidney Sadel on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DiaryOfACEOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
